d1a004e4909459d18b4c81a32e50d61cdfebb969
sergey__azarenko <sergey.azarenko@valtech.com>
2020-02-26 14:18:28 +0200

    change the file read/write layer to support utf-BOM format

---
+++ find/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace find
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 3)
+                return;
+
+            string baseFolder = args[0];
+            string fileFilter = args[1];
+            string pattern = args[2];            
+
+            Regex re = new Regex(pattern, RegexOptions.Singleline | RegexOptions.IgnoreCase);
+            Regex fileFilterRe = new Regex(fileFilter, RegexOptions.Singleline | RegexOptions.IgnoreCase);
+
+            Find(baseFolder, fileFilterRe, re);
+        }
+        static void Find(string baseFolder, Regex fileFilterRe, Regex re)
+        {
+            foreach (string filePath in Directory.GetFiles(baseFolder))
+            {
+                if (!fileFilterRe.IsMatch(filePath))
+                    continue;
+
+                bool isMatch = false; 
+
+                using (StreamReader sr = new StreamReader(filePath))                
+                {
+                    int linenumber = 0;
+                    while (!sr.EndOfStream)
+                    {
+                        linenumber++;
+                        string line = sr.ReadLine();
+                        if (re.IsMatch(line))
+                        {
+                            isMatch = true;
+                            //Console.WriteLine(string.Format("\t{0}: {1}", linenumber, line));
+                        }                        
+                    }
+                }
+
+                if (isMatch)
+                {
+                    Console.WriteLine(filePath);
+                }
+            }
+
+            foreach (string folderPath in Directory.GetDirectories(baseFolder))
+            {
+                Find(folderPath, fileFilterRe, re);
+            }
+        }
+    }
+}
---
+++ replace/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace replace
+{
+    class Program
+    {
+        static Encoding utf8 = new UTF8Encoding(true);
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 4)
+                return;
+
+            string baseFolder = args[0];
+            string fileFilter = args[1];
+            string pattern = args[2];
+            string formatter = args[3];
+
+            Regex re = new Regex(pattern, RegexOptions.Singleline | RegexOptions.IgnoreCase);
+
+            Replace(baseFolder, fileFilter, re, formatter);
+        }
+
+        static void Replace(string baseFolder, string fileFilter, Regex re, string formatter)
+        {
+            foreach (string filePath in Directory.GetFiles(baseFolder))
+            {
+                if (!Regex.IsMatch(filePath, fileFilter))
+                    continue;                
+
+                string tmpfilepath = filePath + "_";
+                bool isMatch = false;
+
+                using (FileStream sr = new FileStream(filePath, FileMode.Open))
+                using (FileStream sw = new FileStream(tmpfilepath, FileMode.OpenOrCreate))
+                {
+                    byte[] buffer = new byte[sr.Length];
+                    sr.Read(buffer, 0, buffer.Length);
+
+                    string data = utf8.GetString(buffer);
+                    if (re.IsMatch(data))
+                    {
+                        byte[] outbuffer = utf8.GetBytes(re.Replace(data, formatter));
+                        sw.Write(outbuffer, 0, outbuffer.Length);
+                        isMatch = true;
+                    }                    
+                }
+
+                if (isMatch)
+                {
+                    Console.WriteLine(filePath);
+                    File.Delete(filePath);
+                    File.Move(tmpfilepath, filePath);
+                }
+                else
+                {
+                    File.Delete(tmpfilepath);
+                }
+            }
+
+            foreach (string folderPath in Directory.GetDirectories(baseFolder))
+            {
+                Replace(folderPath, fileFilter, re, formatter);
+            }
+        }
+    }
+}
